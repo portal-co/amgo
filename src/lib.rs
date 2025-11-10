ab5a3d32fbf9fb4e99e71ac8eac0ee0ed9ba27f3
Graham Kelly <gkgoat6700@gmail.com>
2025-11-10 09:58:24 -0500

    make printable

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 #![no_std]
 pub const AMOGUS: &'static str = "ඞ";
+pub const TAG: u32 = 0xc0;
 pub fn amgo<'a>(a: &'a str, key: &str) -> Option<impl Iterator<Item = u8> + use<'a>> {
     let (_, a) = a.split_once(AMOGUS)?;
     let a = a.strip_prefix(key)?;
     let a = a.strip_prefix(AMOGUS)?;
     Some(a.chars().filter_map(|c| match c as u32 {
-        a if a > 0x21 => Some(((a - 0x21) & 0xff) as u8),
+        a if a > TAG => Some(((a - TAG) & 0xff) as u8),
         _ => None,
     }))
 }
@@ -14,5 +15,5 @@ pub fn demgo(key: &str, a: impl Iterator<Item = u8>) -> impl Iterator<Item = cha
         .chars()
         .chain(key.chars())
         .chain(AMOGUS.chars())
-        .chain(a.filter_map(|a| char::from_u32((a as u32) + 0x21)))
+        .chain(a.filter_map(|a| char::from_u32((a as u32) + TAG)))
 }
