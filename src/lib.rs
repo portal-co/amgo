2d501ce24f0f8ff1aa87dd94668f7cc1bed8d9d3
Graham Kelly <gkgoat6700@gmail.com>
2025-11-10 09:55:14 -0500

    init

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![no_std]
+pub const AMOGUS: &'static str = "ඞ";
+pub fn amgo<'a>(a: &'a str, key: &str) -> Option<impl Iterator<Item = u8> + use<'a>> {
+    let (_, a) = a.split_once(AMOGUS)?;
+    let a = a.strip_prefix(key)?;
+    let a = a.strip_prefix(AMOGUS)?;
+    Some(a.chars().filter_map(|c| match c as u32 {
+        a if a > 0x21 => Some(((a - 0x21) & 0xff) as u8),
+        _ => None,
+    }))
+}
+pub fn demgo(key: &str, a: impl Iterator<Item = u8>) -> impl Iterator<Item = char> {
+    AMOGUS
+        .chars()
+        .chain(key.chars())
+        .chain(AMOGUS.chars())
+        .chain(a.filter_map(|a| char::from_u32((a as u32) + 0x21)))
+}
